57b065526aafef3adce2e542a475419110e5994e
Codexavion <codexavion@protonmail.com>
2022-01-16 14:15:30 -0500

    Added readme info and tested products.json class

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## This is my Readme
 
 ### I am creating a database using just JSON files and a helper method / wrapper function
+
+### Repositories hold helper files for the creation of json storage files created by main.js
+
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const usersRepo = require('./repositories/users');
-
+const productsRepo = require('./repositories/products');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// User login front-end
 
 app.get('/', (req, res) => {
     res.send(`
@@ -21,6 +23,8 @@ app.get('/', (req, res) => {
         </div>
     `);
 });
+
+// User create account - 'submit' button fn - (first state adds user to ./repositories/users.json)
 
 app.post('/', async(req, res) => {
     const { email, password, passwordConfirmation } = req.body;
@@ -42,6 +46,51 @@ app.post('/', async(req, res) => {
     res.send('Account Created!!!');
 });
 
+
+// {
+//     "product": "Flat Rolled Steel",
+//     "type": "Galvanized",
+//     "grade": "Grade-80",
+//     "defects": "none",
+//     "weight": 46000,
+//     "id": "185d9def"
+// }
+
+app.get('/products', (req, res) => {
+    res.send(`
+        <div>
+        
+            <form method="POST">
+                <input name="product" placeholder="product" />
+                <input name="type" placeholder="type" />
+                <input name="grade" placeholder="grade" />
+                <input name="defects" placeholder="defects" />
+                <input name="weight" placeholder="weight" />
+    
+                <button>Add Product</button>
+            </form>
+        </div>
+    `);
+});
+
+app.post('/products', async(req, res) => {
+    const { product, type, grade, defects, weight } = req.body;
+
+    const existingProduct = await productsRepo.getOneBy({ product });
+    if (existingProduct) {
+        return res.send('Product already added');
+    }
+
+
+    // Create a product in our repo
+    const prod = await productsRepo.create({ product, type, grade, defects, weight });
+
+    // Store that id inside of a cookie for session auth
+    res.send('Product added!!!');
+});
+
+
+// App starts on port http://localhost:3000 localhost port #3000
 
 app.listen(3000, () => {
     console.log("listening");
